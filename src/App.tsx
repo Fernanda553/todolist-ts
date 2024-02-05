07b94954e10a1e89d7c734966044971a035f21a5
fernanda nuñez <fernanda.nunez55@gmail.com>
2024-02-05 19:12:21 -0300

    list all and add styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,31 @@
+import { useState } from 'react'
+import { Todos } from './components/Todos'
+
+const allMocks = [
+  {
+    id: '1',
+    title: 'Todo 1',
+    completed: true
+  },
+  {
+    id: '2',
+    title: 'Todo 2',
+    completed: false
+  },
+  {
+    id: '3',
+    title: 'Todo 3',
+    completed: false
+  }
+]
+
 const App = (): JSX.Element => {
+  const [todos, setTodos] = useState(allMocks)
+
   return (
-    <h1>list todo</h1>
+    <div className='todoapp'>
+      <Todos todos={todos}/>
+    </div>
   )
 }
 
